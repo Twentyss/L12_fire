6d57332f85694c5d9af77dfbae666957326b9662
Ilya <pershin.ilya31@gmail.com>
2020-06-07 23:54:47 +0300

    lab12

---
+++ Unit1.h
@@ -0,0 +1,54 @@
+//---------------------------------------------------------------------------
+
+#ifndef Unit1H
+#define Unit1H
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include "GLBaseClasses.hpp"
+#include "GLBehaviours.hpp"
+#include "GLCadencer.hpp"
+#include "GLCoordinates.hpp"
+#include "GLCrossPlatform.hpp"
+#include "GLFireFX.hpp"
+#include "GLGeomObjects.hpp"
+#include "GLObjects.hpp"
+#include "GLScene.hpp"
+#include "GLWin32Viewer.hpp"
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.Menus.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TGLSceneViewer *GLSceneViewer1;
+	TGLScene *GLScene1;
+	TGLTorus *Torus1;
+	TGLSphere *Sphere1;
+	TGLLightSource *GLLightSource2;
+	TGLCamera *GLCamera1;
+	TGLCadencer *GLCadencer1;
+	TGLFireFXManager *GLFireFXManager1;
+	TTimer *Timer1;
+	TMainMenu *MainMenu1;
+	TMenuItem *Help1;
+	TMenuItem *About1;
+	void __fastcall FormMouseWheel(TObject *Sender, TShiftState Shift, int WheelDelta,
+          TPoint &MousePos, bool &Handled);
+	void __fastcall Timer1Timer(TObject *Sender);
+	void __fastcall GLSceneViewer1MouseMove(TObject *Sender, TShiftState Shift, int X,
+          int Y);
+	void __fastcall GLSceneViewer1MouseDown(TObject *Sender, TMouseButton Button, TShiftState Shift,
+          int X, int Y);
+	void __fastcall Help1Click(TObject *Sender);
+	void __fastcall About1Click(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
